a0e8705e0af0d7b3af85289a210099f7f8dac896
Dahbi <54712900+dleila@users.noreply.github.com>
2019-10-29 08:58:03 -0500

    Add files via upload
    Added transposition decryption functions.

--- Crypto.py
+++ Crypto.py
@@ -15,3 +15,18 @@ def scramble2Encrypt(plainText):
         charCount = charCount + 1
     cipherText = oddChars + evenChars
     return cipherText
+
+def scramble2Decrypt(cipherText):
+    halfLength = len(cipherText) // 2
+    evenChars = cipherText[halfLength:]
+    oddChars = cipherText[:halfLength]
+    plainText = ""
+
+    for i in range(halfLength):
+        plainText = plainText + evenChars[i]
+        plainText = plainText + oddChars[i]
+
+    if len(oddChars) < len(evenChars):
+        plainText = plainText + evenChars[-1]
+
+    return plainText
