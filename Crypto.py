0cd8700ea2df063a7d08d56b68478ca0518ae391
Dahbi <54712900+dleila@users.noreply.github.com>
2019-10-29 09:04:42 -0500

    Add files via upload

--- Crypto.py
+++ Crypto.py
@@ -1,20 +1,21 @@
 # Transposition Cipher
 
 # original: this_is_a_secret_message_that_i_want_to_transmit
-# encrypted: hsi__ertmsaeta__att_rnmtti_sasce_esg_htiwn_otasi
+# encrypted:hsi__ertmsaeta__att_rnmtti_sasce_esg_htiwn_otasi
 
 def scramble2Encrypt(plainText):
     evenChars = ""
     oddChars = ""
     charCount = 0
     for ch in plainText:
-        if evenChars % 2 == 0:
+        if charCount % 2 == 0:
             evenChars = evenChars + ch
         else:
             oddChars = oddChars + ch
         charCount = charCount + 1
     cipherText = oddChars + evenChars
     return cipherText
+
 
 def scramble2Decrypt(cipherText):
     halfLength = len(cipherText) // 2
@@ -30,3 +31,9 @@ def scramble2Decrypt(cipherText):
         plainText = plainText + evenChars[-1]
 
     return plainText
+
+
+def encryptMessage():
+    msg = input("Enter the message to encrypt: ")
+    cipherText = scramble2Encrypt(msg)
+    print("The encrypted message is:", cipherText)
